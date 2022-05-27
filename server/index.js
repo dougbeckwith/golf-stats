086d5589e4bd50132171aec0a0872feb03b365fc
Doug Beckwith <dougcbeckwith@gmail.com>
2022-05-27 09:29:35 -0700

    second commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const dotenv = require('dotenv')
+dotenv.config({path: '../.env'})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`${process.env.TEST}Example app listening on port ${port}`)
 })
